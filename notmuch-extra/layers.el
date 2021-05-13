31780584175364b1b6bde6b59a31e59bc10a5c77
Christian Menard <christian.menard@gmx.de>
2021-05-13 13:26:41 +0200

    add an (empty) notmuch-extra layer

---
+++ notmuch-extra/layers.el
@@ -0,0 +1,7 @@
+;; Copyright (c) 2021 Christian Menard
+;;
+;; Author: Christian Menard <christian@gmx.de>
+;;
+;; This file is not part of GNU Emacs.
+
+(configuration-layer/declare-layer 'notmuch)
---
+++ notmuch-extra/packages.el
@@ -0,0 +1,8 @@
+;; Copyright (c) 2021 Christian Menard
+;;
+;; Author: Christian Menard <christian@gmx.de>
+;;
+;; This file is not part of GNU Emacs.
+
+(defconst notmuch-extra-packages
+  '(notmuch))
