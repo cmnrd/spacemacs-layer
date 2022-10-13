8d69f10d656ff4460ee122e618495c33190d15e6
Christian Menard <christian.menard@tu-dresden.de>
2022-10-13 10:44:59 +0200

    remove org-kanban

--- kanban/funcs.el
+++
@@ -1,2 +0,0 @@
-(defun kanban-get-roam-task-files (_)
-  (org-extra-task-files))
--- kanban/packages.el
+++
@@ -1,7 +0,0 @@
-(defconst kanban-packages '(org-kanban))
-
-(defun kanban/init-org-kanban ()
-  (use-package org-kanban))
-
-(defun kanban/post-init-org-kanban ()
-  (advice-add 'org-kanban//params-files :override #'kanban-get-roam-task-files))
