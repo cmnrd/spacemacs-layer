64c0a2f705cdec66867f8f668121dfcd54256c50
Christian Menard <christian.menard@tu-dresden.de>
2023-01-23 10:47:45 +0100

    customize org priorities

--- org-extra/packages.el
+++ org-extra/packages.el
@@ -5,7 +5,7 @@
 ;; This file is not part of GNU Emacs.
 
 (defconst org-extra-packages
-  '(vulpea org crux org-roam-bibtex))
+  '(vulpea org crux org-roam-bibtex org-fancy-priorities))
 
 (defun org-extra/init-vulpea ()
   (use-package vulpea))
@@ -19,6 +19,17 @@
     :hook (org-roam-mode . org-roam-bibtex-mode)
     :config
     (require 'org-ref)))
+
+(defun org-extra/init-org-fancy-priorities ()
+  (use-package org-fancy-priorities
+    :ensure t
+    :hook
+    (org-mode . org-fancy-priorities-mode)
+    :config
+    (setq org-fancy-priorities-list '("A*" "A" "B" "C" "D")
+          org-highest-priority 1
+          org-default-priority 3
+          org-fancy-priorities-list '((?1 . "A*") (?2 . "A") (?3 . "B") (?4 . "C") (?5 . "D")))))
 
 (defun org-extra/post-init-org ()
   (add-to-list 'org-tags-exclude-from-inheritance "tags")
