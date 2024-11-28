8d3bf81fb7ddc57cd824209f6a42b7f3268d88ce
Christian Menard <christian.menard@gmx.de>
2024-11-28 11:48:14 +0100

    org-ql updates

--- org-extra/packages.el
+++ org-extra/packages.el
@@ -48,18 +48,19 @@
           (todo . " %i %-32(org-extra-agenda-category 32) ")
           (tags . " %i %-32(org-extra-agenda-category 32) ")
           (search . " %i %-32(org-extra-agenda-category 32) ")))
-  )
+  ;; Automatically switch to insert mode when capturing
+  (add-hook 'org-capture-mode-hook 'evil-insert-state 90))
 
 (defun org-extra/init-org-ql ()
   (use-package org-ql
     :ensure t
     :config
-(require 'org-ql-search)
-;; Extends org-ql dynamic blocks by the :scope argument
-;; Use org-extra-ql :scope ... instead of org-ql
-;; source https://github.com/alphapapa/org-ql/pull/239#issuecomment-1213052966
-(cl-defun org-dblock-write:org-extra-ql (params)
-    "Insert content for org-ql dynamic block at point according to PARAMS.
+    (require 'org-ql-search)
+    ;; Extends org-ql dynamic blocks by the :scope argument
+    ;; Use org-extra-ql :scope ... instead of org-ql
+    ;; source https://github.com/alphapapa/org-ql/pull/239#issuecomment-1213052966
+    (cl-defun org-dblock-write:org-extra-ql (params)
+      "Insert content for org-ql dynamic block at point according to PARAMS.
 Valid parameters include:
  :scope    The scope to consider for the Org QL query. This can
             be one of the following:
@@ -98,82 +99,82 @@ Valid parameters include:
 For example, an org-ql dynamic block header could look like:
 
   #+BEGIN: org-ql :query (todo \"UNDERWAY\") :columns (priority todo heading) :sort (priority date) :ts-format \"%Y-%m-%d %H:%M\""
-    (-let* (((&plist :scope :query :columns :sort :ts-format :take) params)
-            (org-extra-update-agenda-files)
-            (query (cl-etypecase query
-                     (string (org-ql--query-string-to-sexp query))
-                     (list  ;; SAFETY: Query is in sexp form: ask for confirmation, because it could contain arbitrary code.
-                      (org-ql--ask-unsafe-query query)
-                      query)))
-            (columns (or columns '(heading todo (priority "P"))))
-            (scope (cond ((and (listp scope) (seq-every-p #'stringp scope)) scope)
-                         ((string-equal scope "org-agenda-files") (org-agenda-files))
-                         ((or (not scope) (string-equal scope "buffer")) (current-buffer))
-                         ((string-equal scope "org-directory") (org-ql-search-directories-files))
-                         (t (user-error "Unknown scope '%s'" scope))))
-            ;; MAYBE: Custom column functions.
-            (format-fns
-             ;; NOTE: Backquoting this alist prevents the lambdas from seeing
-             ;; the variable `ts-format', so we use `list' and `cons'.
-             (list (cons 'todo (lambda (element)
-                                 (org-element-property :todo-keyword element)))
-                   (cons 'heading (lambda (element)
-                                    (cond
-                                     ((and org-id-link-to-org-use-id
-                                           (org-element-property :ID element))
-                                      (org-make-link-string (format "id:%s" (org-element-property :ID element))
-                                                            (org-element-property :raw-value element)))
-                                     ((org-element-property :file element)
-                                      (org-make-link-string (format "file:%s::*%s"
-                                                                    (org-element-property :file element)
-                                                                    (org-element-property :raw-value element))
-                                                            (org-element-property :raw-value element)))
-                                     (t (org-make-link-string (org-element-property :raw-value element)
-                                                              (org-link-display-format
-                                                               (org-element-property :raw-value element)))))
-                                    ))
-                   (cons 'priority (lambda (element)
-                                     (--when-let (org-element-property :priority element)
-                                       (char-to-string it))))
-                   (cons 'deadline (lambda (element)
-                                     (--when-let (org-element-property :deadline element)
+      (-let* (((&plist :scope :query :columns :sort :ts-format :take) params)
+              (org-extra-update-agenda-files)
+              (query (cl-etypecase query
+                       (string (org-ql--query-string-to-sexp query))
+                       (list  ;; SAFETY: Query is in sexp form: ask for confirmation, because it could contain arbitrary code.
+                        (org-ql--ask-unsafe-query query)
+                        query)))
+              (columns (or columns '(heading todo (priority "P"))))
+              (scope (cond ((and (listp scope) (seq-every-p #'stringp scope)) scope)
+                           ((string-equal scope "org-agenda-files") (org-agenda-files))
+                           ((or (not scope) (string-equal scope "buffer")) (current-buffer))
+                           ((string-equal scope "org-directory") (org-ql-search-directories-files))
+                           (t (user-error "Unknown scope '%s'" scope))))
+              ;; MAYBE: Custom column functions.
+              (format-fns
+               ;; NOTE: Backquoting this alist prevents the lambdas from seeing
+               ;; the variable `ts-format', so we use `list' and `cons'.
+               (list (cons 'todo (lambda (element)
+                                   (org-element-property :todo-keyword element)))
+                     (cons 'heading (lambda (element)
+                                      (cond
+                                       ((and org-id-link-to-org-use-id
+                                             (org-element-property :ID element))
+                                        (org-make-link-string (format "id:%s" (org-element-property :ID element))
+                                                              (org-element-property :raw-value element)))
+                                       ((org-element-property :file element)
+                                        (org-make-link-string (format "file:%s::*%s"
+                                                                      (org-element-property :file element)
+                                                                      (org-element-property :raw-value element))
+                                                              (org-element-property :raw-value element)))
+                                       (t (org-make-link-string (org-element-property :raw-value element)
+                                                                (org-link-display-format
+                                                                 (org-element-property :raw-value element)))))
+                                      ))
+                     (cons 'priority (lambda (element)
+                                       (--when-let (org-element-property :priority element)
+                                         (char-to-string it))))
+                     (cons 'deadline (lambda (element)
+                                       (--when-let (org-element-property :deadline element)
+                                         (ts-format ts-format (ts-parse-org-element it)))))
+                     (cons 'scheduled (lambda (element)
+                                        (--when-let (org-element-property :scheduled element)
+                                          (ts-format ts-format (ts-parse-org-element it)))))
+                     (cons 'closed (lambda (element)
+                                     (--when-let (org-element-property :closed element)
                                        (ts-format ts-format (ts-parse-org-element it)))))
-                   (cons 'scheduled (lambda (element)
-                                      (--when-let (org-element-property :scheduled element)
-                                        (ts-format ts-format (ts-parse-org-element it)))))
-                   (cons 'closed (lambda (element)
-                                   (--when-let (org-element-property :closed element)
-                                     (ts-format ts-format (ts-parse-org-element it)))))
-                   (cons 'property (lambda (element property)
-                                     (org-element-property (intern (concat ":" (upcase property))) element)))))
-            (elements (org-ql-query :from scope
-                                    :where query
-                                    :select '(org-element-put-property (org-element-headline-parser (line-end-position)) :file (buffer-file-name))
-                                    :order-by sort)))
-      (when take
-        (setf elements (cl-etypecase take
-                         ((and integer (satisfies cl-minusp)) (-take-last (abs take) elements))
-                         (integer (-take take elements)))))
-      (cl-labels ((format-element
-                   (element) (string-join (cl-loop for column in columns
-                                                   collect (or (pcase-exhaustive column
-                                                                 ((pred symbolp)
-                                                                  (funcall (alist-get column format-fns) element))
-                                                                 (`((,column . ,args) ,_header)
-                                                                  (apply (alist-get column format-fns) element args))
-                                                                 (`(,column ,_header)
-                                                                  (funcall (alist-get column format-fns) element)))
-                                                               ""))
-                                          " | ")))
-        ;; Table header
-        (insert "| " (string-join (--map (pcase it
-                                           ((pred symbolp) (capitalize (symbol-name it)))
-                                           (`(,_ ,name) name))
-                                         columns)
-                                  " | ")
-                " |" "\n")
-        (insert "|- \n")  ; Separator hline
-        (dolist (element elements)
-          (insert "| " (format-element element) " |" "\n"))
-        (delete-char -1)
-        (org-table-align))))))
+                     (cons 'property (lambda (element property)
+                                       (org-element-property (intern (concat ":" (upcase property))) element)))))
+              (elements (org-ql-query :from scope
+                                      :where query
+                                      :select '(org-element-put-property (org-element-headline-parser (line-end-position)) :file (buffer-file-name))
+                                      :order-by sort)))
+        (when take
+          (setf elements (cl-etypecase take
+                           ((and integer (satisfies cl-minusp)) (-take-last (abs take) elements))
+                           (integer (-take take elements)))))
+        (cl-labels ((format-element
+                      (element) (string-join (cl-loop for column in columns
+                                                      collect (or (pcase-exhaustive column
+                                                                    ((pred symbolp)
+                                                                     (funcall (alist-get column format-fns) element))
+                                                                    (`((,column . ,args) ,_header)
+                                                                     (apply (alist-get column format-fns) element args))
+                                                                    (`(,column ,_header)
+                                                                     (funcall (alist-get column format-fns) element)))
+                                                                  ""))
+                                             " | ")))
+          ;; Table header
+          (insert "| " (string-join (--map (pcase it
+                                             ((pred symbolp) (capitalize (symbol-name it)))
+                                             (`(,_ ,name) name))
+                                           columns)
+                                    " | ")
+                  " |" "\n")
+          (insert "|- \n")  ; Separator hline
+          (dolist (element elements)
+            (insert "| " (format-element element) " |" "\n"))
+          (delete-char -1)
+          (org-table-align))))))
